c7e1fb9ee0616914a0b3c25f4c348787f583c537
nacero <norberto.acero@hotmail.com>
2023-11-09 20:45:31 -0500

    Emojis

--- thesis.py
+++ thesis.py
@@ -198,3 +198,5 @@ y = A['CLASE'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=2023)
 CLS = HistGradientBoostingClassifier()
 CLS.fit(X_train, y_train)
+
+🥸🥸🥸🥸
